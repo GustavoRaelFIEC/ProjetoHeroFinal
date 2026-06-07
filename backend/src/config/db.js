a37cedc65ea6c20432ce4c9ea6357b6be3935c70
Rael <gustavo41938@fiec.edu.br>
2026-06-07 19:55:05 -0300

    feat: cria conexão com o banco de dados

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+import mysql from "mysql2";
+
+const pool = mysql.createPool({
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+});
+
+export default pool.promise();
