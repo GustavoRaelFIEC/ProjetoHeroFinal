ea055bff2cf77e887c1472d4529c23bc3d13cbd2
Rael <gustavo41938@fiec.edu.br>
2026-06-08 23:06:13 -0300

    chore: adiciona o dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes/routes");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
