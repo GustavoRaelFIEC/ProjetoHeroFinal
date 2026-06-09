909a372ad84b8e8e7eb5c99fb7f5e2fac641fe3c
Rael <gustavo41938@fiec.edu.br>
2026-06-08 23:15:21 -0300

    feat: cria um middleware de cadasatro de usuário "Recrutador"

--- backend/src/controllers/controllerUsers.js
+++ backend/src/controllers/controllerUsers.js
@@ -1,10 +1,41 @@
 const db = require("../config/db");
-const schemaCadastro = require("../schemas/schemaCadastro");
+const { schemaCadastro } = require("../schemas/schemaCadastro");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.listarUsers = async (req, res) => {
   res.send({ message: "Usuários listados com sucesso!" });
 };
 
-exports.cadastrarUser = async (req, res) => {
-  res.send({ message: "Usuários cadastrado com sucesso!" });
+exports.cadastrarUser = async (req, res) => {  
+  try {
+    const result = schemaCadastro.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({ message: result.error });
+    }
+
+    const { nomeCompleto, email, senha } = result.data;
+
+    // RoundSalt
+    const roundSalt = 10;
+
+    const senhaHash = await bcrypt.hash(senha, roundSalt);
+
+    await db.query(
+      "INSERT INTO recrutador (nome_completo, email, senha) VALUES (?, ?, ?)",
+      [nomeCompleto, email, senhaHash],
+    );
+
+    // JWT
+    const token = jwt.sign({ id: result.data.email }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(201).json({ message: "Usuário cadastrado com sucesso!", token });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ message: "Erro interno no servidor" });
+  }
 };
